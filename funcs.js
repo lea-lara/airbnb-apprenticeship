09139d5f0d17077a55ea87ef9aa1bbf6d7e98f99
lea-lara <125421285+lea-lara@users.noreply.github.com>
2023-02-16 14:22:26 -0500

    project files

---
+++ funcs.js
@@ -0,0 +1,15 @@
+
+a = $('#popup')
+    
+    function openPopup(){
+        debugger;
+        a.classList.add('open-popup')
+    }
+    
+    function closePopup()
+    {
+        a.classList.remove('open-popup')
+    }
+
+
+    
